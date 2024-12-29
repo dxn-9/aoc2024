68fbb864fe6c29f6d0a409024311b7ec360bec81
dan <daniel.lauri22@gmail.com>
2024-12-29 01:40:58 +0100

    Day22

--- aoctest/Day22.cs
+++ aoctest/Day22.cs
@@ -10,7 +10,15 @@ public partial class Tests
     {
         var sample = Common.ReadFile("Day22/sample.txt");
         var input = Common.ReadFile("Day22/input.txt");
-        Assert.That(Day22.Solve1(sample), Is.EqualTo(37327623));
-        Assert.That(Day22.Solve1(input), Is.EqualTo(20401393616));
+        // Assert.That(Day22.Solve1(sample), Is.EqualTo(37327623));
+        // Assert.That(Day22.Solve1(input), Is.EqualTo(20401393616));
+        var p2Sample = """
+                       1
+                       2
+                       3
+                       2024
+                       """;
+        Assert.That(Day22.Solve2(p2Sample), Is.EqualTo(23));
+        Assert.That(Day22.Solve2(input), Is.EqualTo(23));
     }
 }
